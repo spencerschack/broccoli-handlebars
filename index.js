df4f327b5ecda9a46903f950b64f3b1544e25397
Spencer Schack <scs1818@gmail.com>
2017-08-08 14:32:35 -0700

    Add noEscape option

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ function HandlebarsWriter(inputNodes, filesGlobs, options) {
     return filename.replace(/(hbs|handlebars)$/, 'html');
   };
   this.handlebars = this.options.handlebars || Handlebars;
+  
+  this.noEscape = !!this.options.noEscape;
 
   this.loadPartials();
   this.loadHelpers();
@@ -72,7 +74,7 @@ HandlebarsWriter.prototype.writeTemplateSync = function(sourceDir, outputPath, t
 
   // load and compile
   var templateStr = fs.readFileSync(path.join(sourceDir, targetFile)).toString();
-  var template = this.handlebars.compile(templateStr);
+  var template = this.handlebars.compile(templateStr, {noEscape: this.noEscape});
 
   fs.writeFileSync(destFilepath, template(context));
 };
